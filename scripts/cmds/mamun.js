73ae1bb6b2c7008a55ac35e5d77ac3821b328443
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-29 12:14:40 +0600

    Update mamun.js

--- scripts/cmds/mamun.js
+++ scripts/cmds/mamun.js
@@ -2,34 +2,34 @@ module.exports = {
   config: {
       name: "mamun",
           version: "1.0.4",
-              author: "〲MAMUNツ࿐ T.T　o.O",
+              author: "〲𝐒𝐇𝐈𝐒𝐇𝐈𝐑ツ࿐ T.T　o.O",
                   role: 0,
-                      shortDescription: "Mamun Profile ",
+                      shortDescription: "𝐒𝐇𝐈𝐒𝐇𝐈𝐑 Profile ",
                           category: "Information",
                               guide: {
-                                    en: "type mamun"
+                                    en: "type 𝐒𝐇𝐈𝐒𝐇𝐈𝐑"
                                         }
                                           },
   onStart: async function () {},
   onChat: async function ({ api, event }) {
       const msg = event.body?.toLowerCase();
-          if (!msg || msg !== "mamun") return;
+          if (!msg || msg !== "𝐒𝐇𝐈𝐒𝐇𝐈𝐑") return;
     const profileText = 
-    `⏤͟͟͞͞𝗠𝗔𝗠𝗨𝗡  𝗜𝗡𝗙𝗢 ☺︎
+    `⏤͟͟͞͞𝐒𝐇𝐈𝐒𝐇𝐈𝐑  𝗜𝗡𝗙𝗢 ☺︎
  ⏤͟͟͞͞☺︎ ┏━━━━━━━━━━━━━━━
- ⏤͟͟͞͞𝐍𝐚𝐦𝐞 ➯ 𝑴𝑨𝑴𝑼𝑵  ⏤͟͟͞͞ ᜊ
+ ⏤͟͟͞͞𝐍𝐚𝐦𝐞 ➯ 𝐒𝐇𝐈𝐒𝐇𝐈𝐑  ⏤͟͟͞͞ ᜊ
  
 ⏤͟͟͞͞𝐍𝐢𝐜𝐤𝐧𝐚𝐦𝐞 ➯⏤͟͟͞͞ 𝐕𝐎𝐍𝐃𝐎 😜
 
 ⏤͟͟͞͞𝐂𝐨𝐮𝐧𝐭𝐫𝐲 ➯⏤͟͟͞͞𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡 🇧🇩
 
-🏤⏤͟͟͞͞𝐇𝐨𝐦𝐞 ➯⏤͟͟͞͞𝐊𝐡𝐮𝐥𝐧𝐚   ☠️
+🏤⏤͟͟͞͞𝐇𝐨𝐦𝐞 ➯⏤͟͟͞͞𝕊𝕚𝕣𝕒𝕛𝕘𝕒𝕟𝕛   ☠️
 
-🏛️⏤͟͟͞͞𝐃𝐢𝐬𝐭𝐫𝐢𝐜𝐭 ➯⏤͟͟͞͞𝐊𝐇𝐔𝐋𝐍𝐀 💀
+🏛️⏤͟͟͞͞𝐃𝐢𝐬𝐭𝐫𝐢𝐜𝐭 ➯⏤͟͟͞͞𝑺𝒊𝒓𝒂𝒋𝒈𝒂𝒏𝒋, 𝑩𝑫 💀
 
-⛪⏤͟͟͞͞𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 ➯ 𝐈𝐧𝐭𝐞𝐫 2𝐧𝐝 𝐘𝐞𝐚𝐫 ✍︎❤️‍🩹
+⛪⏤͟͟͞͞𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 ➯ 𝐈𝐧𝐭𝐞𝐫 1st 𝐘𝐞𝐚𝐫 ✍︎❤️‍🩹
 
-⏤͟͟͞͞𝐀𝐠𝐞 ➯ 20  😊❤️‍🩹
+⏤͟͟͞͞𝐀𝐠𝐞 ➯ 17+  😊❤️‍🩹
 
 🕌⏤͟͟͞͞𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧 ➯ 𝐈𝐬𝐥𝐚𝐦 ❤️♡♡
 
@@ -41,7 +41,7 @@ module.exports = {
 
 
  ⏤͟͟͞͞ ☻ 𝗬𝗢𝗨𝗥 𝗕𝗕𝗭 
- ⏤͟͟͞͞𝑂𝑊𝑁𝐸𝑅⏤☺︎ `;
+ ⏤͟͟͞͞𝑂𝑊𝑁𝐸𝑅⏤☺︎𝐒𝐇𝐈𝐒𝐇𝐈𝐑 `;
     api.sendMessage(profileText, event.threadID, event.messageID);
       }
       };
