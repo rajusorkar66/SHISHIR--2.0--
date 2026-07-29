a8f4a33fa2e5644a38e91b77da63636c37486605
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-29 18:35:39 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -18,8 +18,7 @@ module.exports = {
     const targetThread = args[0] || event.threadID;
 
     try {
-      await api.sendMessage("👋 -শিশির বস থাকো আমার বের করে দিলা তো মনে কষ্ট পাইলাম "! 😄
-ভালো থাকিস এদের খেয়াল রাখিস" ভাবি এর খেয়াল রাখিস! 😅😅...", targetThread);
+      await api.sendMessage("👋 -শিশির বস থাকো আমায় বের করে দিলা তো মনে কষ্ট পাইলাম "! 😄" ভাবি এর খেয়াল রাখিস! 😅😅...", targetThread);
       await api.removeUserFromGroup(botID, targetThread);
     } catch (error) {
       console.error(error);
