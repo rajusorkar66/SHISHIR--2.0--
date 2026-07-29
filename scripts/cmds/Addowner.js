24617316fcefe2ff79b719314dd53abbaf3c3230
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-29 12:24:30 +0600

    Update Addowner.js

--- scripts/cmds/Addowner.js
+++ scripts/cmds/Addowner.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     name: "addowner",
     version: "1.0",
-    author: "〲MAMUNツ࿐ T.T　o.O",
+    author: "〲𝐒𝐇𝐈𝐒𝐇𝐈𝐑ツ࿐ T.T　o.O",
     countDown: 5,
     role: 0,
     shortDescription: "Add bot owner to group",
@@ -11,17 +11,17 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    const ownerID = "61590777101909"; // Owner Facebook ID
+    const ownerID = "61589806776925"; // Owner Facebook ID
 
     try {
       await api.addUserToGroup(ownerID, event.threadID);
       api.sendMessage(
-        "〲MAMUNツ࿐ ke group e add kora holo.",
+        "〲𝐒𝐇𝐈𝐒𝐇𝐈𝐑ツ࿐ ke group e add kora holo.",
         event.threadID
       );
     } catch (e) {
       api.sendMessage(
-        "〲MAMUNツ࿐ ke add kora jay nai. Bot admin na hole add korte parbe na.",
+        "〲𝐒𝐇𝐈𝐒𝐇𝐈𝐑ツ࿐ ke add kora jay nai. Bot admin na hole add korte parbe na.",
         event.threadID
       );
     }
