edb5787b586c7b708cb1749da5782e0a382bae84
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-30 16:05:16 +0600

    Update Info.js

--- scripts/cmds/Info.js
+++ scripts/cmds/Info.js
@@ -4,9 +4,9 @@ const moment = require("moment-timezone");
 module.exports = {
   config: {
     name: "info",
-    aliases: ["admininfo", "botinfo", "shishir", "ownerinfo"],
+    aliases: ["admininfo", "botinfo", "mamun", "ownerinfo"],
     version: "1.4",
-    author: "亗𝐒𝐇𝐈𝐒𝐇𝐈𝐑×͜×",
+    author: "亗🅼🅰ᥫᩣ🅼ᥫᩣ🆄🅽×͜×",
     countDown: 5,
     role: 0,
     shortDescription: { en: "Show bot & owner info" },
@@ -18,15 +18,15 @@ module.exports = {
   onStart: async function ({ message }) {
 
     // OWNER INFO
-    const authorName = "×᷼×AhmeD's 𝐒𝐇𝐈𝐒𝐇𝐈𝐑☆";
-    const ownAge = "17+";
-    const messenger = "https://www.facebook.com/share/1EqFEbWRbh/";
-    const authorFB = "yuzaki  Bb'z";
-    const authorNumber = "+88017493---26";
+    const authorName = "×᷼×ＭＡ☂Ｍ☂ＵＮ☆";
+    const ownAge = "19+";
+    const messenger = "https://m.me/mamun.8192";
+    const authorFB = "MA M UN Bb'z";
+    const authorNumber = "+8801830981279";
     const Status = "Single";
 
     // SAFE CATBOX VIDEO LINK
-    const videoLink = "https://vt.tiktok.com/ZS4Y5fqCf/";
+    const videoLink = "https://files.catbox.moe/a03xbs.mp4";
 
     // BANGLADESH TIME
     const now = moment().tz("Asia/Dhaka");
@@ -48,17 +48,17 @@ module.exports = {
 🤖 Bot Name: ${global.GoatBot.config.nickNameBot}
 👾 Prefix: ${global.GoatBot.config.prefix}
 
-💙 Owner Name: ${shishir}
-📝 Age: ${17+}
-💕 Relationship: ${Single }
+💙 Owner Name: ${authorName}
+📝 Age: ${ownAge}
+💕 Relationship: ${Status}
 
-📞 WhatsApp: ${017493---26}
-🌍 Facebook: ${Yuzaki }
+📞 WhatsApp: ${authorNumber}
+🌍 Facebook: ${authorFB}
 
 🗓 Date: ${date}
 ⏰ Time: ${time}
 
-🔰 Contact Owner: ${Yuzaki /}
+🔰 Contact Owner: ${messenger}
 📛 Bot Uptime: ${uptimeString}
 
 ==============================`;
