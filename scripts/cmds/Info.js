9eb4c8d5d17dffd9bdb31b9870a190503a08f2a2
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-30 16:13:21 +0600

    Update Info.js

--- scripts/cmds/Info.js
+++ scripts/cmds/Info.js
@@ -4,9 +4,9 @@ const moment = require("moment-timezone");
 module.exports = {
   config: {
     name: "info",
-    aliases: ["admininfo", "botinfo", "mamun", "ownerinfo"],
+    aliases: ["admininfo", "botinfo", "shishir", "ownerinfo"],
     version: "1.4",
-    author: "亗🅼🅰ᥫᩣ🅼ᥫᩣ🆄🅽×͜×",
+    author: "亗𝗦𝗛𝗜𝗦𝗛𝗜𝗥×͜×",
     countDown: 5,
     role: 0,
     shortDescription: { en: "Show bot & owner info" },
@@ -18,15 +18,15 @@ module.exports = {
   onStart: async function ({ message }) {
 
     // OWNER INFO
-    const authorName = "×᷼×ＭＡ☂Ｍ☂ＵＮ☆";
+    const authorName = "×᷼×𝗦𝗛𝗜𝗦𝗛𝗜𝗥☆";
     const ownAge = "19+";
-    const messenger = "https://m.me/mamun.8192";
-    const authorFB = "MA M UN Bb'z";
-    const authorNumber = "+8801830981279";
+    const messenger = "https://m.me/shishir.8192";
+    const authorFB = "𝗦𝗛𝗜𝗦𝗛𝗜𝗥 Bb'z";
+    const authorNumber = "+88017493--26";
     const Status = "Single";
 
     // SAFE CATBOX VIDEO LINK
-    const videoLink = "https://files.catbox.moe/a03xbs.mp4";
+    const videoLink = "https://vt.tiktok.com/ZS4YnvcgC/";
 
     // BANGLADESH TIME
     const now = moment().tz("Asia/Dhaka");
