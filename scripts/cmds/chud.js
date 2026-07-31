140fa915471acea9a9f3dc911c16e08f7ef9fcfe
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-07-31 17:11:58 +0600

    Update chud.js

--- scripts/cmds/chud.js
+++ scripts/cmds/chud.js
@@ -19,7 +19,7 @@ module.exports = {
     var arraytag = []; 
         arraytag.push({id: mention, tag: name});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-setTimeout(() => {a({body: "কিরে মাদারচোত আকাশ এর চুদন খাওয়ায় জন্য রেডি তো?" + "লে চুদা খা মাঙ্গের বেটা 😂😂" + name, mentions: arraytag})}, 3000);
+setTimeout(() => {a({body: "কিরে মাদারচোত শিশির বস!  এর চুদন খাওয়ায় জন্য রেডি তো?" + "লে চুদা খা মাঙ্গের বেটা 😂😂" + name, mentions: arraytag})}, 3000);
 setTimeout(() => {a({body: "মাঘি চুদা শরের বাচ্চা কার লগে পঙ্গা নিতে আসছিস 🤬🤬🤬" + " " + name, mentions: arraytag})}, 5000);
 setTimeout(() => {a({body: " চুদে গুহা ফাঁক করে দিব খানকীর পোলা 🤤" + " " + name, mentions: arraytag})}, 7000);
 setTimeout(() => {a({body: " মাদারচোত বোকাচোদা খানকীর পোলা " + " " + name, mentions: arraytag})}, 9000);
