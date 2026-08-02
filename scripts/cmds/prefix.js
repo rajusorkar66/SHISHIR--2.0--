44d540335c98b4d2897e96aafa6964a4089b9ce1
√AhmeD's Shi'shir <rajusorkar6666@gmail.com>
2026-08-02 12:55:03 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -93,7 +93,7 @@ module.exports = {
     const time = moment().tz("Asia/Dhaka").format("hh:mm A");
     const date = moment().tz("Asia/Dhaka").format("DD MMM YYYY");
 
-    const owner = global.GoatBot.config.adminName || "MAMUN";
+    const owner = global.GoatBot.config.adminName || "SHISHIR ";
 
     return message.reply(
 `╭━━━〔 PREFIX 〕━━━╮
